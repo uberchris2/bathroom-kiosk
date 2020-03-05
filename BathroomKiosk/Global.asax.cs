390bbdbd4b8a37c0af7f1a922daa885cabde4d13
Chris Carpenter <chris@uberchris.com>
2020-03-05 16:10:52 -0600

    Force TLS 1.2 for call since wunderground requires it

--- BathroomKiosk/Global.asax.cs
+++ BathroomKiosk/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Net;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +13,8 @@ namespace BathroomKiosk
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         }
     }
 }
